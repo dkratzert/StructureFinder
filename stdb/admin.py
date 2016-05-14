bd69b32b8460cb20beb4f85712def0cd80393b5a
Daniel Kratzert <dkratzert@gmx.de>
2016-05-15 00:52:16 +0200

    improvements

--- stdb/admin.py
+++ stdb/admin.py
@@ -8,18 +8,25 @@ class RefineResultInline(admin.StackedInline):
     extra = 0
     verbose_name = 'Refine Result'
     verbose_name_plural = 'Refine Results'
+    #fields = [
+    #     'is_publishable'
+    #]
+    #inlines = [UnitCell]
+    #fieldsets = [
+     #   ('Unit Cell', {'fields': ['cell']}),
+    #]
 
 class DatasetAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Measurement', {'fields': ['name', 'measure_date', 'operator', 'flask_name', 'machine']}),
     ]
     inlines = [RefineResultInline]
-    list_display = ('name', 'measure_date', 'was_measured_recently', 'machine')
+    list_display = ('name', 'measure_date', 'was_measured_recently', 'publishable', 'machine')
     list_filter = ['measure_date']
     search_fields = ['name', 'operator', 'flask_name']
 
 admin.site.register(Dataset, DatasetAdmin)
-#admin.site.register(RefineResult)
+admin.site.register(RefineResult)
 
 """
 Add possibility to add machines like Choices in https://docs.djangoproject.com/en/1.9/intro/tutorial07/
--- stdb/models.py
+++ stdb/models.py
@@ -1,8 +1,10 @@
 import datetime
 
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
+from django.forms import fields
 from django.utils import timezone
 
 
@@ -12,6 +14,12 @@ class Dataset(models.Model):
     machine = models.CharField(max_length=200)
     operator = models.CharField(max_length=200)
     measure_date = models.DateTimeField('date measured')
+    crystal_size_x = models.FloatField(max_length=4, default=0,
+                                       validators = [MinValueValidator(0), MaxValueValidator(500)])
+    crystal_size_y = models.FloatField(max_length=4, default=0,
+                                       validators=[MinValueValidator(0), MaxValueValidator(500)])
+    crystal_size_z = models.FloatField(max_length=4, default=0,
+                                       validators=[MinValueValidator(0), MaxValueValidator(500)])
 
     def __str__(self):
         return self.name
@@ -24,17 +32,31 @@ class Dataset(models.Model):
     was_measured_recently.boolean = True
     was_measured_recently.short_description = 'Measured recently?'
 
+    def publishable(self):
+        return True
+        #return RefineResult.objects.get(id('name'), is_publishable=True)
+
+    publishable.boolean = True
+
+
 
 class RefineResult(models.Model):
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, default=0)
-    struct_name = models.CharField(max_length=200)
-    cell_a = models.FloatField(max_length=8, default=0)
+    is_publishable = models.BooleanField(default=False)
+    is_publishable.short_description = 'Is the structure publishable?'
+    cell_a = models.FloatField(max_length=8, default=0, verbose_name='a', validators = [MinValueValidator(0), MaxValueValidator(500)])
+    cell_b = models.FloatField(max_length=8, default=0, validators=[MinValueValidator(0), MaxValueValidator(500)])
+    cell_c = models.FloatField(max_length=8, default=0, validators=[MinValueValidator(0), MaxValueValidator(500)])
+    cell_alpha = models.FloatField(max_length=8, default=0, validators = [MinValueValidator(0), MaxValueValidator(180)])
+    cell_beta = models.FloatField(max_length=8, default=0, validators = [MinValueValidator(0), MaxValueValidator(180)])
+    cell_gamma = models.FloatField(max_length=8, default=0, validators = [MinValueValidator(0), MaxValueValidator(180)])
     cell_a.short_description = 'Unit Cell Parameter a'
-    cell_b = models.FloatField(max_length=8, default=0)
     cell_b.short_description = 'Unit Cell Parameter b'
-    cell_c = models.FloatField(max_length=8, default=0)
     cell_c.short_description = 'Unit Cell Parameter c'
+    cell_alpha.short_description = 'Unit Cell Parameter alpha'
+    cell_beta.short_description = 'Unit Cell Parameter beta'
+    cell_gamma.short_description = 'Unit Cell Parameter gamma'
 
     def __str__(self):
-        return self.struct_name
+        return self.dataset.name+' dataset '+str(self.id)
 
