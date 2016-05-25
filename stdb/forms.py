310cb6345aa7e892a69f49d2de8437507daac140
Daniel Kratzert <dkratzert@gmx.de>
2016-05-25 10:21:42 +0200

    jquery and datepicker

--- stdb/forms.py
+++ stdb/forms.py
@@ -5,12 +5,24 @@ from django import forms
 
 
 #from .models import Document
+from django.forms import DateInput
+
 from stdb.models import Dataset
 
 class DateInput(forms.SelectDateWidget):
+    """
+    creates a date widget to select the date for
+    e.g. the measurement
+    """
     input_type = 'date'
-    past_year = datetime.date.today().year-15
-    year_field = range(int(past_year), int(datetime.date.today().year) + 3)
+
+    def __init__(self):
+        super(DateInput, self).__init__()
+        past_year = datetime.date.today().year - 15
+        year_field = range(int(past_year), int(datetime.date.today().year) + 3)
+        self.years = year_field
+
+
 
 class DocumentForm(forms.ModelForm):
     cif_file = forms.FileField(label='Cif file', allow_empty_file=True, required=False)
