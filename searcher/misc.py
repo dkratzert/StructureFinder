fa5c3fef5d38c4120249713d475c7906496da2d9
Daniel Kratzert <dkratzert@gmx.de>
2017-05-30 14:42:42 +0200

    bgcolors

--- searcher/misc.py
+++ searcher/misc.py
@@ -11,6 +11,15 @@ Created on 09.02.2015
 @author: daniel
 """
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def find_binary_string(file, string, seek, size, return_ascii=False):
     """
