57d8563fe4c14eef9bc97c9e46cc8ccf6c061fd4
Daniel Kratzert <dkratzert@gmx.de>
2017-06-01 09:37:50 +0200

    more stuff

--- opengl/moleculegl.py
+++ opengl/moleculegl.py
@@ -7,21 +7,19 @@ from PyQt5 import Qt3DCore, QtCore
 from PyQt5 import Qt3DExtras
 from PyQt5 import Qt3DInput
 from PyQt5 import Qt3DRender
-from PyQt5.QtCore import pyqtSignal, QPoint, QSize, Qt, QObject
-from PyQt5.QtGui import QColor, QOpenGLVersionProfile, QSurfaceFormat, QVector3D, QGuiApplication
-from PyQt5.QtOpenGL import QGLWidget
-from PyQt5.QtWidgets import QOpenGLWidget
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QVector3D, QGuiApplication
 
 
-
-class OrbitTransformController(QObject):
+class OrbitTransformController(Qt3DCore.QTransform):
     targetChanged = pyqtSignal(int)
     radiuschanged = pyqtSignal(int)
+    angleChanged = pyqtSignal(int)
 
     def __init__(self, transform):
         super().__init__()
         self.m_target = None
-        self.m_radius = 0
+        self.m_radius = 1
         self.m_angle = 0
         self.m_matrix = transform.matrix()
 
@@ -30,11 +28,17 @@ class OrbitTransformController(QObject):
             self.m_target = target
             self.targetChanged.emit(target)
 
+    def target(self):
+        return self.m_target
+
     def setRadius(self, radius):
-        if QtCore.qFuzzyCompare(radius, self.m_radius):
+        if not QtCore.qFuzzyCompare(radius, self.m_radius):
             self.m_radius = radius
             self.updateMatrix()
             self.targetChanged.emit(radius)
+
+    def radius(self):
+        return self.m_radius
 
     def updateMatrix(self):
         self.m_matrix.setToIdentity()
@@ -42,6 +46,16 @@ class OrbitTransformController(QObject):
         self.m_matrix.translate(self.m_radius, 0.0, 0.0)
         self.m_target.setMatrix(self.m_matrix)
 
+    def setAngle(self, angle):
+        print('###angle')
+        if not QtCore.qFuzzyCompare(angle):
+            self.m_angle = angle
+            self.updateMatrix()
+            self.angleChanged.emit(angle)
+
+    def angle(self):
+        print('###angle2')
+        return self.m_angle
 
 
 def createScene():
@@ -56,6 +70,20 @@ def createScene():
     controller = OrbitTransformController(sphereTransform)
     controller.setTarget(sphereTransform)
     controller.setRadius(5.0)
+    print('###rotation: ###')
+    sphereRotateTransformAnimation = QtCore.QPropertyAnimation(sphereTransform)
+    sphereRotateTransformAnimation.setTargetObject(controller)
+    sphereRotateTransformAnimation.setPropertyName(b"angle")
+    sphereRotateTransformAnimation.setStartValue(0)
+    sphereRotateTransformAnimation.setEndValue(360)
+    sphereRotateTransformAnimation.setDuration(10000)
+    sphereRotateTransformAnimation.setLoopCount(-1)
+    sphereRotateTransformAnimation.Paused = 0
+    sphereRotateTransformAnimation.Stopped = 0
+    sphereRotateTransformAnimation.Forward = 1
+    sphereRotateTransformAnimation.Backward = 0
+    sphereRotateTransformAnimation.start()
+
     print('##2')
     sphereEntity.addComponent(sphereMesh)
     sphereEntity.addComponent(sphereTransform)
