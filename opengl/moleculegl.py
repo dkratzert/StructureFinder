80591fb4935b3380f1e03ab50f02917cc5fbb559
Daniel Kratzert <dkratzert@gmx.de>
2017-06-16 11:04:31 +0200

    trying bonds

--- opengl/moleculegl.py
+++ opengl/moleculegl.py
@@ -8,6 +8,8 @@ from PyQt5 import Qt3DExtras
 from PyQt5 import Qt3DInput
 from PyQt5 import Qt3DRender
 #from PyQt5.Qt3DRender.QPickEvent import RightButton
+from PyQt5.Qt3DCore import QEntity
+from PyQt5.Qt3DRender import QPointLight
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QGuiApplication, QQuaternion, QVector3D, QColor
 
@@ -73,12 +75,12 @@ class OrbitTransformController(Qt3DCore.QComponent):
         return self.m_angle
 
 
+
 class MyScene(Qt3DCore.QEntity):
     def __init__(self, *arg, **args):
         super(MyScene, self).__init__()
 
     def createScene(self):
-        rootEntity = Qt3DCore.QEntity()
         """
         material = Qt3DExtras.QPhongAlphaMaterial(rootEntity)
         material.setAmbient(QColor('gray'))
@@ -102,15 +104,53 @@ class MyScene(Qt3DCore.QEntity):
         F2    3    0.129932   -0.463873    0.548990
         F3    3    0.055789   -0.388160    0.454486
         """
+        mid = QVector3D(0.0, 0.0, 0.0)
+        rootEntity = Qt3DCore.QEntity()
+        multvec = QVector3D(2, 2, 2)
+        cololist = ['gray', 'red', 'blue', 'gray', 'green', 'green', 'green', 'green']
+        #atlist = [QVector3D(0.0906, -0.3034, 0.5138), QVector3D(0.0452, -0.2525, 0.5619), QVector3D(0.1299, -0.4638, 0.5489),
+        #          QVector3D(0.0557, -0.3881, 0.4544)]
+        atlist = "2.85359    2.63232    0.48588; 2.01243    2.18253    1.10514; 0.92388    1.60218    1.87814; " \
+                 "0.20347   -1.99886    9.07608; -0.70305   -1.66389    9.92535; 0.78718   -3.05595    9.69676; " \
+                 "-0.23894   -2.55716    8.02754"
+        atlist = atlist.split(';')
+        atlist = [QVector3D(*[float(x) for x in i.split()]) for i in atlist]
+        mid = mid * (-1.0 / len(atlist))
+        """
         #C1
-        self.add_sphere(rootEntity, QVector3D(0.0906, -0.3034, 0.5138)*QVector3D(10, 10, 10), 'gray')
+        self.add_sphere(rootEntity, QVector3D(0.0906, -0.3034, 0.5138)*multvec, 'gray')
         #F1
-        self.add_sphere(rootEntity, QVector3D(0.0452, -0.2525, 0.5619)*QVector3D(10, 10, 10), 'red')
+        self.add_sphere(rootEntity, QVector3D(0.0452, -0.2525, 0.5619)*multvec, 'red')
         #F2
-        self.add_sphere(rootEntity, QVector3D(0.1299, -0.4638, 0.5489)*QVector3D(10, 10, 10), 'blue')
+        self.add_sphere(rootEntity, QVector3D(0.1299, -0.4638, 0.5489)*multvec, 'blue')
         #F3
-        self.add_sphere(rootEntity, QVector3D(0.0557, -0.3881, 0.4544)*QVector3D(10, 10, 10), 'green')
+        """
+        for n, at in enumerate(atlist):
+            at = at*multvec
+            self.add_sphere(rootEntity, at, cololist[n])
+            self.add_bond(rootEntity, at, atlist[0]*multvec, 'gray')
         return rootEntity
+
+    def add_bond(self, rootEntity, pos1, pos2, color):
+        """
+        https://github.com/GarageGames/Qt/blob/master/qt-5/qt3d/examples/qt3d/custom-mesh-cpp/main.cpp
+        """
+        material = Qt3DExtras.QPhongMaterial(rootEntity)
+        material.setAmbient(QColor('gray'))
+        # Torus:
+        cylinderEntity = Qt3DCore.QEntity(rootEntity)
+        cylinderMesh = Qt3DExtras.QCylinderMesh()
+        cylinderMesh.setRadius(0.2)
+        cylinderMesh.setLength(20)
+        cylinderMesh.setRings(100)
+        cylinderMesh.setSlices(20)
+        cylinderTransform = Qt3DCore.QTransform()
+        cylinderTransform.setScale3D(QVector3D(1, 1, 1.0))
+        cylinderTransform.setTranslation(pos1)
+        cylinderTransform.setRotation(QQuaternion.fromAxisAndAngle(pos2, 25.0))
+        cylinderEntity.addComponent(cylinderMesh)
+        cylinderEntity.addComponent(cylinderTransform)
+        cylinderEntity.addComponent(material)
 
     def add_sphere(self, rootEntity, position, colour):
         """ 
@@ -132,10 +172,10 @@ class MyScene(Qt3DCore.QEntity):
         print('rad:')
         # controller.setRadius(1.0)
         print('##2')
-        sphereEntity.addComponent(sphereMesh)
         sphereEntity.addComponent(sphereTransform)
+        sphereEntity.addComponent(sphereMesh)
         sphereEntity.addComponent(material)
-        sphereTransform.matrix().setToIdentity()
+        #sphereTransform.matrix().setToIdentity()
 
     def event(self, event):
         handled = QtWidgets.QOpenGLWidget.event(event)
@@ -158,25 +198,35 @@ if __name__ == '__main__':
     app = QGuiApplication(sys.argv)
     view = Qt3DExtras.Qt3DWindow()
     s = MyScene()
-    scene = s.createScene()
+    rootEntity = s.createScene()
     print('#scene')
     # // Camera
     camera = view.camera()
-    #lens = Qt3DRender.QCameraLens()
+    lens = Qt3DRender.QCameraLens()
     #camera.lens().setPerspectiveProjection(45.0, 16.0 / 9.0, 0.1, 1000.0)
-    camera.lens().setOrthographicProjection(-26.0, 26.0, -29.0, 29.0, -1.0, 600.0)
+    lens.setOrthographicProjection(-50, 50.0, -50.0, 50.0, -1.0, 500.0)
     camera.setUpVector(QVector3D(0, 1.0, 0))
-    camera.setPosition(QVector3D(0, 0, 40.0))  # Entfernung
+    camera.setPosition(QVector3D(0, 0, 100.0))  # Entfernung
     camera.setViewCenter(QVector3D(0, 0, 0))
+    #camera.setfieldOfView = 45
+    #print(camera.fieldOfView())
     print('#camera')
     # // For camera controls
-    camController = Qt3DExtras.QOrbitCameraController(scene)
+    camController = Qt3DExtras.QOrbitCameraController(rootEntity)
     camController.setLinearSpeed(-30.0)
     camController.setLookSpeed(-480.0)
     camController.setCamera(camera)
-    camController.setZoomInLimit(1)
+    #camController.setZoomInLimit(1)
+    lightEntity = QEntity(rootEntity)
+    light = QPointLight(lightEntity)
+    light.setColor(QColor("white"))
+    light.setIntensity(0.5)
+    lightEntity.addComponent(light)
+    lightTransform = Qt3DCore.QTransform(lightEntity)
+    lightTransform.setTranslation(QVector3D(0.5, 0.5, 0))
+    #lightEntity.addComponent(lightTransform)
+    view.setRootEntity(rootEntity)
 
-    view.setRootEntity(scene)
     print('view#')
     view.show()
     app.exec()
