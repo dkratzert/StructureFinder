17ed1fcca9e909e46e8b5231488d0f4b1b6ae9eb
Daniel Kratzert <dkratzert@gmx.de>
2017-08-23 15:29:17 +0200

    more template stuff

--- cgi/cgi-bin/strflog.py
+++ cgi/cgi-bin/strflog.py
@@ -11,6 +11,8 @@ from wsgiref.util import setup_testing_defaults, FileWrapper
 
 import pathlib
 
+from searcher import database_handler
+
 cgitb.enable(display=1, logdir="./log")
 
 
@@ -26,12 +28,31 @@ def application(environ, start_response):
     return [txt]
 
 
-def process_data():
+def process_data(dbfilename="../structuredb.sqlite"):
     """
+    Structure.Id             0
+    Structure.measurement    1
+    Structure.path           2
+    Structure.filename       3
+    Structure.dataname       4
     """
-    p = pathlib.Path("cgi/strflog_Template.htm")
+    structures = database_handler.StructureTable(dbfilename)
+    if not structures:
+        return
+    table_string = ""
+    for i in structures.get_all_structure_names():
+        line = '<tr> <td>{0}</td> <td>{1}</td> <td>{2}</td> <td>{3}</td> </tr>\n'\
+                .format(i[3].decode('ascii', errors='ignore'),
+                        i[4].decode('ascii', errors='ignore'),
+                        i[2].decode('ascii', errors='ignore'),
+                        i[0]
+                        )
+        # i[0] -> id
+        table_string += line
+        table_string = table_string
+    p = pathlib.Path("strflog_Template.htm")
     t = Template(p.read_bytes().decode('ascii', 'ignore'))
-    return str(t.safe_substitute({"logtablecolumns":"9"})).encode('ascii', 'ignore')
+    return str(t.safe_substitute({"logtablecolumns": table_string})).encode('ascii', 'ignore')
 
 
 if __name__ == "__main__":
