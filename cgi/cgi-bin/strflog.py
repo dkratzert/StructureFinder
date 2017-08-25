db3ed0869d7649f6883444179dda010625da848a
Daniel Kratzert <dkratzert@gmx.de>
2017-08-25 09:31:07 +0200

    cleannup

--- cgi/cgi-bin/strflog.py
+++ cgi/cgi-bin/strflog.py
@@ -1,22 +1,16 @@
 #!C:\tools\Python-3.6.2_64\python.exe
 ##!/usr/local/bin/python3
 
-import os
-import pprint
-from html import escape
-from urllib import parse
-from string import Template
-import cgitb
-import datetime
-import sys
-from wsgiref.util import setup_testing_defaults, FileWrapper
-
 import pathlib
+from string import Template
+from urllib import parse
+from wsgiref.util import setup_testing_defaults
 
 from lattice import lattice
 from searcher import database_handler
 
-cgitb.enable(display=1, logdir="./log")
+
+# cgitb.enable(display=1, logdir="./log")
 
 
 def application(environ, start_response):
@@ -32,12 +26,12 @@ def application(environ, start_response):
     # When the method is POST the variable will be sent
     # in the HTTP request body which is passed by the WSGI server
     # in the file like wsgi.input environment variable.
-    #pprint.pprint(environ)
+    # pprint.pprint(environ)
     request_body = environ['wsgi.input'].read(request_body_size).decode('utf-8')
     d = parse.parse_qs(request_body)
     if d.get("cell"):
         cell = (d.get("cell"))
-    #pprint.pprint('request_body:', d)
+    # pprint.pprint('request_body:', d)
     status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
     start_response(status, headers)
@@ -45,6 +39,7 @@ def application(environ, start_response):
     structures = database_handler.StructureTable(dbfilename)
     txt = process_data(structures)
     return [txt]
+
 
 def find_cell(cellstr: str):
     """
@@ -55,21 +50,22 @@ def find_cell(cellstr: str):
     except (TypeError, ValueError):
         return []
     if len(cell) != 6:
-        #self.statusBar().showMessage('Not a valid unit cell!', msecs=3000)
+        # self.statusBar().showMessage('Not a valid unit cell!', msecs=3000)
         # self.show_full_list()
         return []
-    #if self.ui.moreResultsCheckBox.isChecked() or \
+    # if self.ui.moreResultsCheckBox.isChecked() or \
     #        self.ui.ad_moreResultscheckBox.isChecked():
     threshold = 0.08
     ltol = 0.09
     atol = 1.8
-    #else:
+    # else:
     #    threshold = 0.03
     #    ltol = 0.001
     #    atol = 1
-    #try:
+    # try:
     volume = lattice.vol_unitcell(*cell)
-    #idlist = structures.find_by_volume(volume, threshold)
+    # idlist = structures.find_by_volume(volume, threshold)
+
 
 def process_data(structures):
     """
@@ -83,23 +79,24 @@ def process_data(structures):
         return []
     table_string = ""
     for i in structures.get_all_structure_names():
-        line = '<tr> <td>{0}</td> <td>{1}</td> <td>{2}</td> </tr>\n'\
-                .format(i[3].decode('ascii', errors='ignore'),
-                        i[4].decode('ascii', errors='ignore'),
-                        i[2].decode('ascii', errors='ignore'),
-                        #i[0]
-                        )
+        line = '<tr> <td>{0}</td> <td>{1}</td> <td>{2}</td> </tr>\n' \
+            .format(i[3].decode('utf-8', errors='ignore'),
+                    i[4].decode('utf-8', errors='ignore'),
+                    i[2].decode('utf-8', errors='ignore'),
+                    # i[0]
+                    )
         # i[0] -> id
         table_string += line
         table_string = table_string
     p = pathlib.Path("./cgi/strflog_Template.htm")
     t = Template(p.read_bytes().decode('utf-8', 'ignore'))
-    return str(t.safe_substitute({"logtablecolumns": table_string})).encode('utf-8', 'ignore')#.decode('utf-8')
+    return str(t.safe_substitute({"logtablecolumns": table_string})).encode('utf-8', 'ignore')
 
 
 if __name__ == "__main__":
     try:
         import wsgiref.simple_server
+
         server = wsgiref.simple_server.make_server('127.0.0.1', 8000, application)
         server.serve_forever()
         print("Webserver running...")
