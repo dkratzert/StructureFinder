22584787ca27e2961b731949537177044579ed1a
Daniel Kratzert <dkratzert@gmx.de>
2017-08-29 17:56:07 +0200

    mac stuff

--- mac_copyfiles.sh
+++ mac_copyfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo cp -r cgi/jasny-bootstrap /Library/WebServer/CGI-Executables/
-sudo cp -v cgi/cgi-bin/strflog.py /Library/WebServer/CGI-Executables/strflog.cgi
-sudo cp -v cgi/strflog_Template.htm /Library/WebServer/Documents/
+sudo cp -r cgi_ui/jasny-bootstrap /Library/WebServer/CGI-Executables/
+sudo cp -v cgi_ui/cgi-bin/strflog.py /Library/WebServer/CGI-Executables/strflog.cgi
+sudo cp -v cgi_ui/strflog_Template.htm /Library/WebServer/Documents/
