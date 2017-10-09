79a58ff3c92c9c1e8937dd8abb39c52012cc4198
Daniel Kratzert <dkratzert@gmx.de>
2017-10-09 08:47:33 +0200

    copy script windows

--- scripts/copy_windows_web.cmd
+++ scripts/copy_windows_web.cmd
@@ -3,6 +3,7 @@ copy /Y D:\GitHub\StructureFinder\cgi_ui\strf_web_Template.htm D:\downloads\http
 
 copy /Y D:\GitHub\StructureFinder\cgi_ui\cgi-bin\strf_web.py D:\downloads\httpd-2.4.27-x64-vc14\Apache24\cgi-bin\strf_web.cgi
 
-copy /Y D:\GitHub\StructureFinder\searcher\database_handler.py D:\downloads\httpd-2.4.27-x64-vc14\Apache24\cgi-bin\searcher\database_handler.py
+copy /Y D:\GitHub\StructureFinder\searcher\*.py D:\downloads\httpd-2.4.27-x64-vc14\Apache24\cgi-bin\searcher\
+copy /Y D:\GitHub\StructureFinder\misc\*.py D:\downloads\httpd-2.4.27-x64-vc14\Apache24\cgi-bin\misc\
 
 copy /Y D:\GitHub\StructureFinder\displaymol\JSmol_dk.nojq.lite.js D:\downloads\httpd-2.4.27-x64-vc14\Apache24\htdocs\jsmol
