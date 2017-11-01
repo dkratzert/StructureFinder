204ae2fbe187fb5be8121117e492f852239cb0bc
Daniel Kratzert <dkratzert@gmx.de>
2017-11-01 14:51:43 +0100

    foo

---
+++ cif/cif.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 01.11.2017
+
+ ----------------------------------------------------------------------------
+* "THE BEER-WARE LICENSE" (Revision 42):
+* <daniel.kratzert@uni-freiburg.de> wrote this file. As long as you retain this
+* notice you can do whatever you want with this stuff. If we meet some day, and
+* you think this stuff is worth it, you can buy me a beer in return.
+* ----------------------------------------------------------------------------
+
+@author: daniel
+"""
+
+with open('../test-data/p-1_a.cif', 'r') as f:
+    ciflines = f.readlines()
+
+if __name__ == '__main__':
+    pass
+    #import doctest
+    #failed, attempted = doctest.testmod()  # verbose=True)
+    #if failed == 0:
+    #    print('passed all {} tests!'.format(attempted))
