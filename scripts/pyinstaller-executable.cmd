b0c8201e54d46e840bb86ae0209d3b4239a5aeff
Daniel Kratzert <dkratzert@gmx.de>
2018-01-10 09:58:33 +0100

    64bit installer

--- scripts/pyinstaller-executable.cmd
+++ scripts/pyinstaller-executable.cmd
@@ -1,2 +1,2 @@
 cd ..
-c:\tools\Python-3.6.2_64\Scripts\pyinstaller.exe -c --add-data="gui;gui" --add-data="displaymol;displaymol" --add-data="icons;icons" -n StructureFinder -w -y -i "icons/strf.ico" strf.py
+c:\tools\Python-3.6.2_64\Scripts\pyinstaller.exe --clean --add-data="gui;gui" --add-data="displaymol;displaymol" --add-data="icons;icons" -n StructureFinder -w -y -i "icons/strf.ico" strf.py
