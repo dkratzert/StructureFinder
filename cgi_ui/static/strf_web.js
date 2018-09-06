1804337331868198ae3ea2ffa1b01ea77a9303af
Daniel Kratzert <dkratzert@gmx.de>
2018-09-06 09:46:46 +0200

    small fix

--- cgi_ui/static/strf_web.js
+++ cgi_ui/static/strf_web.js
@@ -178,7 +178,7 @@ $(document).ready(function($){
                 if (files[0].name.split('.').pop() === 'p4p') {
                     get_cell_from_p4p(txt);
                 }
-                if (files[0].name.split('.').pop() === 'res|ins') {
+                if (files[0].name.split('.').pop().match('res|ins')) {
                     get_cell_from_res(txt);
                 }
                 if (files[0].name.split('.').pop() === 'cif') {
