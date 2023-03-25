7112326ea6b78dc3fc7965f670c09fa100af8c91
Daniel Kratzert <dkratzert@gmx.de>
2023-03-25 17:40:40 +0100

    removes old junk

--- scripts/win/stdb.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-TITLE "Structurefinder - XP version"
-
-SET args=%*
-
-cls
-start .\pythonw.exe -S stdb.py %args%;exit
--- scripts/win/strf.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-TITLE "Structurefinder"
-
-start pythonw.exe strf.py
-rem pause
-exit
