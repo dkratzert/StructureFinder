f400538ad7e0965fc20683ee948aceae6a897fd1
Daniel Kratzert <dkratzert@gmx.de>
2023-04-28 10:25:32 +0200

    more tests

---
+++ tests/test_strf_tools.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+
+class TestGetValue(unittest.TestCase):
+    def test_get_value_empty_brackets(self):
+        self.assertEqual(1.123, get_value('1.123()'))
+
+    def test_get_value_with_error_value(self):
+        self.assertEqual(1.123, get_value('1.123(56)'))
+
+    def test_get_value_no_open_bracket(self):
+        self.assertRaises(ValueError, get_value, '1.123)')
+
+    def test_get_value_no_closing_bracket(self):
+        self.assertEqual(1.123, get_value('1.123('))
+
+
+if __name__ == "__main__":
+    unittest.main()
