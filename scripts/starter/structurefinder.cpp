59a0058e4be00c1d613cad23e9e1e46d116e4724
Daniel Kratzert <dkratzert@gmx.de>
2023-09-01 10:56:34 +0200

    add files for exe

---
+++ scripts/starter/structurefinder.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#ifdef _WIN32
+#include <windows.h>
+#include "structurefinder.h"
+#endif
+
+
+int main(int argc, char* argv[]) {
+	
+	cout << "Starting StructureFinder..." << endl;
+
+#ifdef _WIN32
+	_putenv_s("PYTHONPATH", ".");
+
+	bool hide = true;
+	for (int i = 1; i < argc; ++i) {
+		if (strcmp(argv[i], "-nohide") == 0) {
+			hide = false;
+			argv[i] = "";
+			break;
+		}
+	}
+	if (hide) {
+		HWND hWnd = GetConsoleWindow();
+		ShowWindow(hWnd, SW_HIDE);  // Hide the console window
+	}
+#else
+	setenv("PYTHONPATH", ".", 1);
+#endif
+	string command = "venv\\Scripts\\python structurefinder/strf.py";
+	add_argvalues(command, argc, argv);
+	// cout << command << endl;
+	system(command.c_str());
+	return 0;
+}
+
+void add_argvalues(string& command, int argc, char* argv[])
+{
+	for (int i = 1; i < argc; ++i) {
+		command += " ";
+		command += argv[i];
+	}
+}
+
+
+
