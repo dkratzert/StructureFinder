5d10ea579ae3a6eb1dcd8e743ab95c6294c191a3
Daniel Kratzert <dkratzert@gmx.de>
2025-04-11 14:56:17 +0200

    Update _make_win_release.py

--- scripts/_make_win_release.py
+++ scripts/_make_win_release.py
@@ -12,11 +12,11 @@ main_path = str(Path(__file__).resolve().parent)
 pathadd = [app_path, main_path, str(Path(app_path) / 'src')]
 sys.path.extend(pathadd)
 
-from scripts.compile_ui_files import compile_ui  # noqa: E402
-from scripts.version_numbers import disable_debug, isspath, process_iss, pypath  # noqa: E402
-from structurefinder.misc.version import VERSION  # noqa: E402
+from scripts.compile_ui_files import compile_ui
+from scripts.version_numbers import disable_debug, isspath, process_iss, pypath
+from structurefinder.misc.version import VERSION
 
-print("Updating version numbers to version {} ...".format(VERSION))
+print(f"Updating version numbers to version {VERSION} ...")
 
 for i in isspath:
     process_iss(i)
@@ -41,10 +41,10 @@ def sha512_checksum(filename, block_size=65536):
 
 def make_shasum(filename):
     sha = sha512_checksum(filename)
-    shafile = Path('scripts/Output/StructureFinder-setup-x64-v{}-sha512.sha'.format(VERSION))
+    shafile = Path(f'scripts/Output/StructureFinder-setup-x64-v{VERSION}-sha512.sha')
     shafile.unlink(missing_ok=True)
     shafile.write_text(sha)
-    print("SHA512: {}".format(sha))
+    print(f"SHA512: {sha}")
 
 
 def make_installer():
@@ -52,7 +52,7 @@ def make_installer():
     innosetup_compiler2 = r'C:\Program Files (x86)\Inno Setup 6/ISCC.exe'
     if not Path(innosetup_compiler).exists():
         innosetup_compiler = innosetup_compiler2
-    subprocess.run([innosetup_compiler, '/Qp', f'/dMyAppVersion={VERSION}', r'scripts\strf-install_win64.iss', ])
+    subprocess.run([innosetup_compiler, '/Qp', f'/dMyAppVersion={VERSION}', r'scripts\strf-install_win64.iss', ], check=False)
 
 
 def compile_python_files():
@@ -62,14 +62,14 @@ def compile_python_files():
 
 
 if __name__ == '__main__':
-    compile_ui()
+    #compile_ui()
     compile_python_files()
     # Make binary distributions:
     make_installer()
 
-    make_shasum("scripts/Output/StructureFinder-setup-x64-v{}.exe".format(VERSION))
+    make_shasum(f"scripts/Output/StructureFinder-setup-x64-v{VERSION}.exe")
 
-    print('\nCreated version: {}'.format(VERSION))
+    print(f'\nCreated version: {VERSION}')
     print(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
 
-    subprocess.call("scripts/Output/StructureFinder-setup-x64-v{}.exe".format(VERSION))
+    subprocess.call(f"scripts/Output/StructureFinder-setup-x64-v{VERSION}.exe")
