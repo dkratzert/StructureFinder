43c0ed524308228094f48e6ddc3d54f75b737e46
Daniel Kratzert <dkratzert@gmx.de>
2026-04-13 13:03:26 +0200

    Update imports to use fastmolwidget and adjust Python version requirement in pyproject.toml

--- src/structurefinder/gui/strf_main.py
+++ src/structurefinder/gui/strf_main.py
@@ -1653,7 +1653,7 @@ class Ui_stdbMainwindow(object):
         self.actionCopy_Unit_Cell.setText(_translate("stdbMainwindow", "Copy Unit Cell"))
         self.actionGo_to_All_CIF_Tab.setText(_translate("stdbMainwindow", "Go to All CIF Tab"))
         self.actionGo_to_All_CIF_Tab.setShortcut(_translate("stdbMainwindow", "Meta+A"))
-from structurefinder.displaymol.molecule2D import MoleculeWidget
+from fastmolwidget.molecule2D import MoleculeWidget
 from structurefinder.gui.table_view import StructuresListTableView
 
 
